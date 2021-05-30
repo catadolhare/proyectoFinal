54569558459b0f52b1e1ab21a4813f04014485e3
CatalinaDolhare <cdolhare@udesa.edu.ar>
2021-05-30 17:11:00 -0300

    update del main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 let seccionAlbum = document.getElementById('seccionAlbum')
 let seccionArtista = document.getElementById('seccionArtista')
 let seccionCancion = document.getElementById('seccionCancion')
+let recomArtista = document.getElementById('recomArtista')
+let recomAlbum = document.getElementById('recomAlbum')
+let recomCancion = document.getElementById('recomCancion')
+
 fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
 .then(respuesta =>{
     return respuesta.json()
@@ -30,7 +34,18 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart')
         console.log(dataCharts.tracks.data[i].artist.name);
         console.log(dataCharts.tracks.data[i].artist.picture_big);
         seccionCancion.innerHTML = `<article class="eleccion"><a href="detailCancion.html"><img src="${dataCharts.tracks.data[i].picture_big}" alt="${dataCharts.tracks.data[i].title}"><ul class="info"><li><h2>${dataCharts.tracks.data[i].title}</h2></li><li><p>${dataCharts.tracks.data[i].artist.name}</p></li></ul></a></article>`
-
+    }
+    //Recomendaciones Artista
+    for(let i=0; i<3; i++){
+        recomArtista.innerHTML = `<article class="rec"><a href="detailArtista.html"><img src="${dataCharts.artists.data[i].picture_big}" alt="${dataCharts.artists.data[i].name}"><h3>${dataCharts.artists.data[i].name}</h3></a></article>`
+    }
+    //Recomendaciones Albumes
+    for(let i=0; i<3; i++){
+        recomAlbum.innerHTML = `<article class="rec"><a href="detailAlbum.html"><img src="${dataCharts.albums.data[i].cover_big}" alt="${dataCharts.albums.data[i].title}"><h3>${dataCharts.albums.data[i].title}</h3></a></article>`
+    }
+    //Recomendaciones Cancion
+    for(let i=0; i<3; i++){
+        recomCancion.innerHTML = `<article class="rec"><a href="detailCancion.html"><img src="${dataCharts.tracks.data[i].artist.picture_big}" alt="${dataCharts.tracks.data[i].artist.name}"><h3>${dataCharts.tracks.data[i].title}</h3></a></article>`
     }
 })
 .catch(error => console.log(error))

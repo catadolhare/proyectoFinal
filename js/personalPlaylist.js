8c6b35b1c0df401a248d5738cd234070805023c4
CatalinaDolhare <cdolhare@udesa.edu.ar>
2021-06-09 20:37:55 -0300

    update de playlist

--- js/personalPlaylist.js
+++ js/personalPlaylist.js
@@ -1,9 +1,9 @@
 let cancionAlbumFav = localStorage.getItem('cancionesFavAlbum')
-
 let arrayFavAlbum = JSON.parse(cancionAlbumFav)
 let listaCanciones = document.querySelector('.playlist')
 let play = document.querySelector('.play')
 let vacio = document.getElementById('vacio')
+let eliminarPlaylist = document.getElementById('eliminar')
 
 if(arrayFavAlbum.length === 0){
     play.style.display = 'none'
@@ -20,9 +20,22 @@ if(arrayFavAlbum.length === 0){
                 <ul class="elegido">
                     <li><a href="detailCancion.html?id=${dataAlbum.tracks.data[i].id}"><h4>${dataAlbum.tracks.data[i].title}</h4></a></li>
                     <li><a href="detailAlbum.html?id=${dataAlbum.id}">${dataAlbum.title}</a> | <a href="detailArtista.html?id=${dataAlbum.artist.id}">${dataAlbum.artist.name}</a></li>
-                </ul></article>`
+                    <li id="borrarAlbum"><a href="#"><i class="far fa-trash-alt"></i></a></li>
+                </ul></article>` 
             }
         })
         .catch(error => console.log(error))
     }
 }
+let borrarAlbum = document.getElementById('borrarAlbum')
+borrarAlbum.addEventListener('click', function(e){
+    e.preventDefault();
+    let cancionAlbumPosicion = arrayFavAlbum.tracks.data.indexOf(id);
+    arrayFavAlbum.splice(cancionAlbumPosicion, 1);
+
+})
+console.log(localStorage);
+eliminarPlaylist.addEventListener('click', function(e){
+    e.preventDefault();
+    localStorage.clear(cancionFavAlbum)
+})

c5522d6af5e080072ae3935098cf716df83f830a
CatalinaDolhare <cdolhare@udesa.edu.ar>
2021-06-11 13:52:39 -0300

    update del personalPlaylist

--- js/personalPlaylist.js
+++ js/personalPlaylist.js
@@ -1,11 +1,15 @@
 let cancionAlbumFav = localStorage.getItem('cancionesFavAlbum')
 let arrayFavAlbum = JSON.parse(cancionAlbumFav)
+
+let cancionFav = localStorage.getItem('cancionesFavoritas')
+let arrayCancionFav = JSON.parse(cancionFav)
+
 let listaCanciones = document.querySelector('.playlist')
 let play = document.querySelector('.play')
 let vacio = document.getElementById('vacio')
 let eliminarPlaylist = document.getElementById('eliminar')
 
-if(arrayFavAlbum.length === 0){
+if(arrayFavAlbum.length === 0 && arrayCancionFav.length === 0){
     play.style.display = 'none'
 } else {
     vacio.style.display = 'none'
@@ -26,8 +30,23 @@ if(arrayFavAlbum.length === 0){
         })
         .catch(error => console.log(error))
     }
+    for(let i=0; i<arrayCancionFav.length; i++){
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${arrayCancionFav}`)
+        .then(respuesta =>{
+            return respuesta.json()
+        })
+        .then(dataCancion =>{
+            listaCanciones.innerHTML += `<article class="favorito"><img src="${dataCancion.album.cover_big}" alt="Album ${dataCancion.album.title}">
+                <ul class="elegido">
+                    <li><a href="detailCancion.html?id=${dataCancion.id}"><h4>${dataCancion.title}</h4></a></li>
+                    <li><a href="detailAlbum.html?id=${dataCancion.album.id}">${dataCancion.album.title}</a> | <a href="detailArtista.html?id=${dataCancion.artist.id}">${dataCancion.artist.name}</a></li>
+                    <li id="borrarAlbum"><a href="#"><i class="far fa-trash-alt"></i></a></li>
+                </ul></article>`
+        })
+        .catch(error => console.log(error))
+    }
 }
-let borrarAlbum = document.getElementById('borrarAlbum')
+/*let borrarAlbum = document.getElementById('borrarAlbum')
 borrarAlbum.addEventListener('click', function(e){
     e.preventDefault();
     let cancionAlbumPosicion = arrayFavAlbum.tracks.data.indexOf(id);
@@ -38,4 +57,4 @@ console.log(localStorage);
 eliminarPlaylist.addEventListener('click', function(e){
     e.preventDefault();
     localStorage.clear(cancionFavAlbum)
-})
+})*/

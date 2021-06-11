c70b8f1149c0398bad358a1bc78f750079073932
CatalinaDolhare <cdolhare@udesa.edu.ar>
2021-06-11 13:43:43 -0300

    update de detailCancion.js

--- js/detailCancion.js
+++ js/detailCancion.js
@@ -17,3 +17,37 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${id}`)
     artista.innerHTML = `<a href="detailArtista.html?id=${dataCancion.artist.id}">${dataCancion.artist.name}</a>`
     album.innerHTML = `<a href="detailAlbum.html?${dataCancion.album.id}">${dataCancion.album.title}</a>`
 })
+.catch(error => console.log(error))
+
+//Agregar cancion a favoritos
+let agregarSacar = document.getElementById('agregarSacar')
+let cancionesFavoritas = []
+
+let traerCancionesFavoritas = localStorage.getItem('cancionesFavoritas')
+let traerCancionesAlbum = localStorage.getItem('cancionesFavAlbum')//me trae el id del album
+console.log(cancionesFavoritas);
+
+if(traerCancionesFavoritas != null){
+    cancionesFavoritas = JSON.parse(traerCancionesFavoritas)
+}
+
+if(cancionesFavoritas.includes(id)){
+    agregarSacar.innerHTML = 'Eliminar de mi playlist'
+}
+
+agregarSacar.addEventListener('click',function(e){
+    e.preventDefault()
+
+    if(cancionesFavoritas.includes(id)){
+        let cancionesFavPosicion = cancionesFavoritas.indexOf(id);
+        cancionesFavoritas.splice(cancionesFavPosicion, 1);
+        agregarSacar.innerHTML = 'Agregar a mi playlist'
+    } else {
+        cancionesFavoritas.push(id)
+        agregarSacar.innerHTML = 'Eliminar de mi playlist'
+    }
+
+    let cancionFavString = JSON.stringify(cancionesFavoritas)
+    localStorage.setItem('cancionesFavoritas', cancionFavString)
+    console.log(localStorage);
+})

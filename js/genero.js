1552c8cb80fba0129bdf0a73b6b2509109e9b2ff
RenataLamas <renatalamas8@gmail.com>
2021-06-24 20:31:38 -0300

    (resultado de busqueda terminado)

--- js/genero.js
+++ js/genero.js
@@ -5,9 +5,6 @@ let columna2 = document.querySelector('.columna2')
 
 
 
-
-
-
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre`)
 .then(respuesta =>{
     return respuesta.json();
@@ -15,7 +12,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre`)
 .then(datagenero =>{
     console.log(datagenero);
     for( i=1 ; i<7; i++){ //pongo igual a 1 porque la primera imagen dice "todos"
-        columna2.innerHTML += `<article class = "fotogenero" ><a href="detailGenero.html?id=${datagenero.data[i].id }" <h3 class="h3"> ${datagenero.data[i].name}  </h3> <img src = "${datagenero.data[i].picture}"  </article>`
+        columna2.innerHTML += `<article class = "fotogenero" ><a href="detailGenero.html?id=${datagenero.data[i].id }" <h3 class="h3"> ${datagenero.data[i].name}  </h3> <img src = "${datagenero.data[i].picture}" </article>`
   // le estoy pasando por la a un id
     }
 
--- js/searchResults.js
+++ js/searchResults.js
@@ -4,7 +4,7 @@ let buscar = busqueda.get('buscar')
  console.log(buscar);
 let resultados = document.querySelector('.resultados')
 let contenido = ''
-let titulo = document.querySelector('.titulogonchy')
+let titulo = document.querySelector('.titulob')
 
 titulo.innerHTML += buscar
 //titulo.classList.add('tituloBusqueda')
@@ -20,7 +20,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${bus
  console.log(dataBusqueda);
    for(let i=0; i < dataBusqueda.data.length; i++){
        contenido += `<article class="contenido2">
-                        <p class="titulosCanciones"><a href="detail-track.html?id=${dataBusqueda.data[i].id}"> ${dataBusqueda.data[i].title} </a></p>
+                        <p class="Resultados"><a href="detailCancion.html?id=${dataBusqueda.data[i].id}"> ${dataBusqueda.data[i].title} </a></p>
                     </article>`
    }
    resultados.innerHTML += contenido

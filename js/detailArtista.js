98c906c689f19709c94a313199091582f2f4f67f
RenataLamas <renatalamas8@gmail.com>
2021-06-24 15:21:55 -0300

    (artista js casi terminado, detalle de busqueda casi terminado)

--- js/detailArtista.js
+++ js/detailArtista.js
@@ -4,10 +4,34 @@ let segundafila = document.querySelector('.segundafila')
 let section = document.querySelector('.section')
 let imagendeartista = document.querySelector('.imagendeartista')
 let nombredeartista = document.querySelector('.fila1')
+let fila2= document.querySelector('.fila2')
 let fila3 = document.querySelector('.fila3')
 let fila4 = document.querySelector('.fila4')
 //console.log(fila4);
 console.log(id);
+
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top?limit=50`)
+.then(respuesta => {
+    return respuesta.json()
+})
+.then(playartist=>{
+console.log(playartist);
+for(let i=0 ; i<1; i++){
+    fila2.innerHTML +=  `<article class="article1"> 
+    <h2 class="F"> <a href="${playartist.data[0].preview}"}"><h6 class="F">PLAY ARTIST<h6></a></h2>
+ </article>
+ <article class="article2">
+     <h5> Listeners</h5>
+     <h4>18.7k</h4>
+ </article>
+  <article class="article3">
+      <h5>scrobbles</h5>
+      <h4>580M</h4>
+  </article>`
+}
+})
+
+
 
 //NOMBRE DEL ARTISTA  Y FOTO, terminado. SI anda
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`)
@@ -16,26 +40,48 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}`)
 })
 .then(detalleartista => {
     console.log(detalleartista);
-    imagendeartista.innerHTML += `<img src="${detalleartista.picture_big}" alt=""></img>`
+    imagendeartista.innerHTML += `<img class="fotoart" src="${detalleartista.picture_big}" alt=""></img>`
     nombredeartista.innerHTML += `<article class="nombre">  <h1>${detalleartista.name}</h1></article>`
 
 })
+.catch(error => console.log(error))
+
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`)
+.then(respuesta => {
+    return respuesta.json()
+})
+.then(cancionartista => {
+    for(let i=0 ; i<1 ; i++){
+        fila3.innerHTML  += ` <article class="article4">
+        <iframe width="200" height="140" src="https://www.deezer.com/track/${cancionartista.data[0].link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       </article>
+       <article class="article5">
+        <h5> Popular esta semana</h5>
+        <a href="detailCancion.html?id=${cancionartista.data[0].id}"><h4 class="F">${cancionartista.data[0].title}</h4></a>
+        <h5> ${cancionartista.data[0].rank} Listeners</h5>
+       </article>`
+    }
+    console.log(cancionartista); // Me devuelve bien los datos 
+    
+   } )
 .catch(error => console.log(error))
 
 
 
 
 
-// ALBUM DESTACADO DEL ARTISTA Y EL NOMBRE DEL ALBUM  , aca tengo el problema!
+
 fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`)
 .then(respuesta => {
     return respuesta.json()
 })
 .then(albumartista => {
-    console.log(albumartista); // Me devuelve bien los datos 
-    fila4 += ` <figure class="albumH"> <img src="${albumartista.cover}" alt="fineline" class="imagendeharry"></figure> 
-    <article class="article7"><h5> Ultimo lanzamiento</h5> <h4>${albumartista.title}</h4><a href="detailAlbum.html?id=${albumartista.id}"><h6 class="F">VER ALBUM<h6></a></article>`
-    // no me aparece en el browser 
+    for(let i=0; i<1 ; i++) {
+        fila4.innerHTML += ` <figure class="albumH"> <img class="fotoalbumH" src="${albumartista.data[0].cover}" alt="fineline" class="imagendeharry"></figure> 
+    <article class="article7"><h5> Ultimo lanzamiento</h5> <h3>${albumartista.data[0].title}</h3><a href="detailAlbum.html?id=${albumartista.data[0].id}"><h6 class="F">VER ALBUM<h6></a></article>`
+    }
+    console.log(albumartista);  
+    
    } )
 .catch(error => console.log(error))
 
@@ -50,7 +96,7 @@ fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/t
     
     //console.log(topcinco.data);
     //console.log(topcinco.data +'------');
-   for( let i=0 ; i<6; i++){ 
+   for( let i=0 ; i<7; i++){ 
     segundafila.innerHTML += `<article class="topcinco">
         <a href="detailCancion.html?id=${topcinco.data[i].id}">
             <img src="${topcinco.data[i].picture}" alt="${topcinco.data[i].name}" class="imagencancion">
---
+++ js/searchResults.js
@@ -0,0 +1,32 @@
+let busqueda = new URLSearchParams(location.search) /* CAPTURO New URLSearchParams me trae los datos de mejor manera - con location.search mandando a buscar lo que typeo el usuario- Aca tengo lo que el usuario typeo */
+let buscar = busqueda.get('buscar') /* OBTENGO EL DATO Extraigo lo que el usuario typeo*/
+let resultados = document.querySelector('.resultados')
+let contenido = ''
+let titulo = document.querySelector('.titulo')
+
+titulo.innerHTML += buscar
+titulo.classList.add('tituloBusqueda')
+
+
+
+
+fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${buscar}`)
+.then(respuesta =>{
+   return respuesta.json()
+})
+.then(dataBusqueda =>{ /en dataBusqueda estan los datos/
+   console.log(dataBusqueda);
+   for(let i=0; i < dataBusqueda.data.length; i++){
+       contenido += `<article class="contenido2">
+                        <p class="titulosCanciones"><a href="detail-track.html?id=${dataBusqueda.data[i].id}"> ${dataBusqueda.data[i].title} </a></p>
+                    </article>`
+   }
+   resultados.innerHTML += contenido
+   if(dataBusqueda.data.length == 0){
+      alert('La busqueda no trajo resultados')
+   }
+   
+})
+.catch(error =>{
+   console.log(error);
+})
